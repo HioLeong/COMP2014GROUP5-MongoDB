cbec61018069ef1c50feb40ac9d9a211de6f4b03
Michael Leong <Home@Michaels-MacBook-Air.local>
2013-03-10 01:57:20 +0000

    /src removed

--- Mobile Application RESTful API/src/com/comp2013group5/restful/FormResource.java
+++ Mobile Application RESTful API/src/com/comp2013group5/restful/FormResource.java
@@ -1,6 +1,5 @@
 package com.comp2013group5.restful;
 
-import org.json.JSONObject;
 import org.restlet.resource.Get;
 
 import com.comp2014.group5.form_database.FormAccessor;
@@ -10,14 +9,12 @@ public class FormResource extends BaseResource {
 	private static final String IP_STRING = "localhost";
 
 	private String formName;
-	private JSONObject formJSONObject;
 	private FormAccessor formAccessor;
 
 	@Override
 	public void doInit() {
 		formName = (String) getRequestAttributes().get("formName");
 		formAccessor = new FormAccessor(IP_STRING);
-		
 	}
 
 	@Get
